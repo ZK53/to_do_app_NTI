a37257f713bbfb08cf98aaa461f4372d1994a4e4
Mohamed Zakaria <mohamedzakaria532005@gmail.com>
2026-09-11 06:55:57 +0300

    implementign splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:to_do_app/core/utils/colors.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Align(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              "assets/images/splash.svg",
+              height: 344.h,
+              width: 334.w,
+            ),
+            SizedBox(height: 44.h),
+            Text(
+              "TODO",
+              style: TextStyle(
+                color: AppColors.spalshText,
+                fontWeight: FontWeight.w900,
+                fontSize: 36.sp,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
