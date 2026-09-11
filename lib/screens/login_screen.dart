4a0439c3504b41d2fdf0d9460877cf5649552380
Mohamed Zakaria <mohamedzakaria532005@gmail.com>
2026-09-11 10:10:21 +0300

    wrapping screen with singleChildScrollView to avoid overflow

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -20,75 +20,77 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.background,
-      body: Column(
-        children: [
-          Image.asset(
-            "assets/images/flag.png",
-            height: 293.h,
-            width: double.infinity,
-            fit: BoxFit.cover,
-          ),
-          SizedBox(height: 23.h),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 23.w),
-            child: CustomTextField(
-              obsecure: false,
-              text: "Username",
-              prefixIcon: SvgPicture.asset(
-                "assets/images/auth/login_username_icon.svg",
-              ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/flag.png",
+              height: 293.h,
+              width: double.infinity,
+              fit: BoxFit.cover,
             ),
-          ),
-          SizedBox(height: 10.h),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 23.h),
-            child: CustomTextField(
-              obsecure: !isShowen,
-              text: "Password",
-              prefixIcon: SvgPicture.asset(
-                "assets/images/auth/login_password_icon.svg",
-              ),
-              suffixIcon: isShowen
-                  ? "assets/images/auth/login_username_icon_showen.svg"
-                  : "assets/images/auth/login_username_icon_hidden.svg",
-              onPressed: () {
-                setState(() {
-                  isShowen = !isShowen;
-                });
-              },
-            ),
-          ),
-          SizedBox(height: 23.h),
-          CustomButton(onPressed: () {}, text: "Login"),
-          SizedBox(height: 41.h),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Don't have aacount?",
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w300,
-                  color: Colors.black,
+            SizedBox(height: 23.h),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 23.w),
+              child: CustomTextField(
+                obsecure: false,
+                text: "Username",
+                prefixIcon: SvgPicture.asset(
+                  "assets/images/auth/login_username_icon.svg",
                 ),
               ),
-              TextButton(
-                onPressed: () => Navigator.of(context).pushAndRemoveUntil(
-                  MaterialPageRoute(builder: ((context) => RegisterScreen())),
-                  (Route route) => false,
+            ),
+            SizedBox(height: 10.h),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 23.h),
+              child: CustomTextField(
+                obsecure: !isShowen,
+                text: "Password",
+                prefixIcon: SvgPicture.asset(
+                  "assets/images/auth/login_password_icon.svg",
                 ),
-                child: Text(
-                  "Register",
+                suffixIcon: isShowen
+                    ? "assets/images/auth/login_username_icon_showen.svg"
+                    : "assets/images/auth/login_username_icon_hidden.svg",
+                onPressed: () {
+                  setState(() {
+                    isShowen = !isShowen;
+                  });
+                },
+              ),
+            ),
+            SizedBox(height: 23.h),
+            CustomButton(onPressed: () {}, text: "Login"),
+            SizedBox(height: 41.h),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Don't have aacount?",
                   style: TextStyle(
                     fontSize: 14,
-                    fontWeight: FontWeight.w700,
+                    fontWeight: FontWeight.w300,
                     color: Colors.black,
                   ),
                 ),
-              ),
-            ],
-          ),
-        ],
+                TextButton(
+                  onPressed: () => Navigator.of(context).pushAndRemoveUntil(
+                    MaterialPageRoute(builder: ((context) => RegisterScreen())),
+                    (Route route) => false,
+                  ),
+                  child: Text(
+                    "Register",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
