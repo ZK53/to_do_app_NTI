5335ea18e57f44640aa79f345eea11cdec65700c
Mohamed Zakaria <mohamedzakaria532005@gmail.com>
2026-09-11 07:11:59 +0300

    Implementing let's start screen

---
+++ lib/screens/lets_start_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:to_do_app/core/components/custom_button.dart';
+
+class LetsStartScreen extends StatelessWidget {
+  const LetsStartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Align(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              "assets/images/let's_start.svg",
+              height: 301.7.h,
+              width: 342.86.w,
+            ),
+            SizedBox(height: 60.4.h),
+            Text(
+              "Welcome To",
+              style: TextStyle(fontWeight: FontWeight.w400, fontSize: 24.sp),
+            ),
+            Text(
+              "Do It !",
+              style: TextStyle(fontWeight: FontWeight.w400, fontSize: 24.sp),
+            ),
+            SizedBox(height: 40.h),
+            Text(
+              "Ready to conquer your tasks? Let's do",
+              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16.sp),
+            ),
+            Text(
+              "it together.",
+              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16.sp),
+            ),
+            SizedBox(height: 68.h),
+            CustomButton(onPressed: () {}, text: "Let's Start"),
+          ],
+        ),
+      ),
+    );
+  }
+}
