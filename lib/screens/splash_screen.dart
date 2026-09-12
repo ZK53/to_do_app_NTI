95cb1272e968fd03eebd55f35ae0d14eb5bd1445
Mohamed Zakaria <mohamedzakaria532005@gmail.com>
2026-09-12 07:01:22 +0300

    making the spalsh screen a spalsh

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -2,13 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:to_do_app/core/utils/colors.dart';
+import 'package:to_do_app/screens/home_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 3)).then((_) {
+      Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+        ((route) => false),
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: AppColors.background,
+    return Scaffold(
+      backgroundColor: AppColors.background,
       body: Align(
         alignment: Alignment.center,
         child: Column(
