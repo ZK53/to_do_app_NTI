a20cc9790551c578fcc45d451360ed2f5324a575
Mohamed Zakaria <mohamedzakaria532005@gmail.com>
2026-09-12 11:11:08 +0300

    adding some colors to the app

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final Color background = Color(0xFFf3f5f4);
+  static final Color textFieldFill = Color(0xFFCDCDCD);
+  static final Color button = Color(0xFF149954);
+  static final Color spalshText = Color(0xFF149954);
+}
